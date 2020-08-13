061a0eb5cb5d1178dc2cd8fc5100e8954e2f0a60
Diana Gutierrez <loribeth90@hotmail.com>
2020-08-12 19:26:57 -0500

    eliminar

--- veciapi/android/app/src/main/kotlin/com/example/veciapi/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.veciapi
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- veciapi/lib/constants.dart
+++
@@ -1,4 +0,0 @@
-import 'package:flutter/material.dart';
-
-const kPrimaryColor = Color(0xFFEF5350);
-const kPrimaryLightColor = Color(0xFFF1E6FF);
